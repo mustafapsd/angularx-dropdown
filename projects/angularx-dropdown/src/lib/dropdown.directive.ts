353a9369f0fa88b793497f5a34cc2fc5e95934ce
Mustafa Dağlıoğlu <mustafa.psd@yandex.com>
2022-05-20 15:05:29 +0300

    Code review

--- projects/angularx-dropdown/src/lib/dropdown.directive.ts
+++ projects/angularx-dropdown/src/lib/dropdown.directive.ts
@@ -32,7 +32,7 @@ export class DropdownDirective implements OnInit {
   @Input()
   disableCloseOnClickOutside = false;
 
-  private dropDownContainer!: HTMLElement | null;
+  private dropDownContainer!: HTMLDivElement | null;
 
   constructor(
     private elementRef: ElementRef,
@@ -42,6 +42,111 @@ export class DropdownDirective implements OnInit {
 
   ngOnInit(): void {
     // Add css classes to global style
+    this.addStyles();
+  }
+
+  @HostListener('click') onClick() {
+    // If dropdown is already open, close it
+    if (this.dropDownContainer) {
+      this.closeDropdown();
+      return;
+    }
+
+    const dropdownButton = this.elementRef.nativeElement as HTMLElement;
+    const dropdownContainer = document.createElement('div');
+    this.renderer.appendChild(dropdownContainer, this.viewRef.createEmbeddedView(this.dropdownTemplate).rootNodes[0]);
+
+    this.dropDownContainer = dropdownContainer;
+
+    this.addClasses();
+
+    this.alignContainer(dropdownButton);
+
+    this.validateIsIsNotOutside(dropdownButton);
+
+    this.addCloseFunctionToCloseButton();
+
+    this.renderer.appendChild(dropdownButton.parentElement, dropdownContainer);
+
+
+    // Add eventlistener to close dropdown when clicked outside
+    if (!this.disableCloseOnClickOutside) {
+      setTimeout(() => {
+        document.addEventListener('click', this.closeDropdownEvent);
+      }, 100);
+    }
+  }
+
+  private addClasses() {
+    if (this.dropDownContainer) {
+      this.dropDownContainer.classList.add(...[
+        'angularx-dropdown',
+        `angularx-dropdown-${this.size}`,
+      ]);
+
+      if (this.classList) {
+        if (typeof this.classList === 'string') {
+          this.dropDownContainer.classList.add(this.classList);
+        } else {
+          this.dropDownContainer.classList.add(...this.classList);
+        }
+      }
+    }
+  }
+
+  private alignContainer(dropdownButton: HTMLElement) {
+    if (this.dropDownContainer) {
+      this.dropDownContainer.style.top = `${dropdownButton.clientHeight + dropdownButton.offsetTop + 5}px`;
+
+      if (this.alignment === 'right') {
+        this.dropDownContainer.style.left = `${dropdownButton.clientWidth + dropdownButton.offsetLeft}px`;
+        this.dropDownContainer.style.transform = 'translateX(-100%)';
+      } else {
+        this.dropDownContainer.style.left = `${dropdownButton.offsetLeft}px`;
+      }
+    }
+  }
+
+  private validateIsIsNotOutside(dropdownButton: HTMLElement) {
+    if (this.dropDownContainer) {
+      const IS_IT_RIGHT_HAND_SIDE = dropdownButton.offsetLeft > (window.innerWidth / 2);
+
+      if (IS_IT_RIGHT_HAND_SIDE) {
+        this.dropDownContainer.style.maxWidth = `${(window.innerWidth - dropdownButton.offsetLeft) - 20}px`;
+      } else {
+        this.dropDownContainer.style.maxWidth = `${window.innerWidth - dropdownButton.offsetLeft - dropdownButton.clientWidth - 20}px`;
+      }
+    }
+  }
+
+  private addCloseFunctionToCloseButton() {
+    // Control all the elements with for
+    if (this.dropDownContainer) {
+      for (const CHILD of Array.from(this.dropDownContainer.querySelectorAll('*'))) {
+        const ATTRIBUTES = Array.from(CHILD.attributes).map(attr => attr.name);
+        if (ATTRIBUTES.includes('dropdown-close')) {
+          CHILD.addEventListener('click', () => {
+            this.closeDropdown();
+          });
+        }
+      }
+    }
+  }
+
+  // Close dropdown when clicked outside
+  private closeDropdownEvent = (e: MouseEvent) => {
+    if (!this.dropDownContainer?.contains(e.target as Node)) {
+      this.closeDropdown();
+      document.removeEventListener('click', this.closeDropdownEvent);
+    }
+  }
+
+  private closeDropdown() {
+    this.dropDownContainer?.remove();
+    this.dropDownContainer = null;
+  }
+
+  private addStyles() {
     const style = this.renderer.createElement('style');
     this.renderer.appendChild(style, this.renderer.createText(`
         .angularx-dropdown {
@@ -70,85 +175,4 @@ export class DropdownDirective implements OnInit {
 
     this.renderer.appendChild(document.body, style);
   }
-
-  @HostListener('click') onClick() {
-
-    // If dropdown is already open, close it
-    if (this.dropDownContainer) {
-      this.closeDropdown();
-      return;
-    }
-
-    const dropdownButton = this.elementRef.nativeElement as HTMLElement;
-    const dropdownContainer = document.createElement('div');
-    this.renderer.appendChild(dropdownContainer, this.viewRef.createEmbeddedView(this.dropdownTemplate).rootNodes[0]);
-
-
-    dropdownContainer.classList.add(...[
-      'angularx-dropdown',
-      `angularx-dropdown-${this.size}`,
-    ]);
-
-    if (this.classList) {
-      if (typeof this.classList === 'string') {
-        dropdownContainer.classList.add(this.classList);
-      } else {
-        dropdownContainer.classList.add(...this.classList);
-      }
-    }
-
-    dropdownContainer.style.top = `${dropdownButton.clientHeight + dropdownButton.offsetTop + 5}px`;
-
-    if (this.alignment === 'right') {
-      dropdownContainer.style.left = `${dropdownButton.clientWidth + dropdownButton.offsetLeft}px`;
-      dropdownContainer.style.transform = 'translateX(-100%)';
-    } else {
-      dropdownContainer.style.left = `${dropdownButton.offsetLeft}px`;
-    }
-
-    const IS_IT_RIGHT_HAND_SIDE = dropdownButton.offsetLeft > (window.innerWidth / 2);
-
-    if (IS_IT_RIGHT_HAND_SIDE) {
-      dropdownContainer.style.maxWidth = `${(window.innerWidth - dropdownButton.offsetLeft) - 20}px`;
-    } else {
-      dropdownContainer.style.maxWidth = `${window.innerWidth - dropdownButton.offsetLeft - dropdownButton.clientWidth - 20}px`;
-    }
-
-    this.dropDownContainer = dropdownContainer;
-
-    // Control all the elements with for
-
-    for (const CHILD of Array.from(dropdownContainer.querySelectorAll('*'))) {
-      const ATTRIBUTES = Array.from(CHILD.attributes).map(attr => attr.name);
-      if (ATTRIBUTES.includes('dropdown-close')) {
-        CHILD.addEventListener('click', () => {
-          this.closeDropdown();
-        });
-      }
-    }
-
-    this.renderer.appendChild(dropdownButton.parentElement, dropdownContainer);
-
-
-    // Add eventlistener to close dropdown when clicked outside
-    if (!this.disableCloseOnClickOutside) {
-      setTimeout(() => {
-        document.addEventListener('click', this.closeDropdownEvent);
-      }, 100);
-    }
-  }
-
-  // Close dropdown when clicked outside
-  private closeDropdownEvent = (e: MouseEvent) => {
-    if (!this.dropDownContainer?.contains(e.target as Node)) {
-      this.closeDropdown();
-      document.removeEventListener('click', this.closeDropdownEvent);
-    }
-  }
-
-  private closeDropdown() {
-    this.dropDownContainer?.remove();
-    this.dropDownContainer = null;
-  }
-
 }
